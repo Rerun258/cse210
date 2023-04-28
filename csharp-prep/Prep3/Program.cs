1b30e06d7ecaac1ebcd02b033abdfb5cf6d73c1b
McClain <mj.lindsay@live.com>
2023-04-27 18:34:11 -0700

    preps 1-3 are done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 1000);
+        int guess = 0;
+        do
+        {
+            
+            Console.Write("What is the magic number: ");
+            string answer = Console.ReadLine();
+            guess = int.Parse(answer);
+
+            if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess == number)
+            {
+                Console.WriteLine("You Guessed it");
+            }
+        } while (guess != number);
+
     }
 }
