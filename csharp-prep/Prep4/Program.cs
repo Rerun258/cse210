4df9648c30561c570487d12ff9d033949361da6b
McClain <mj.lindsay@live.com>
2023-04-29 12:33:23 -0700

    prep 4-5

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int x = 0;
+        List<int> numbers = new List<int>();
+        int max = 0;
+        int count = 0;
+        int sum = 0;
+
+
+        do 
+        {
+            Console.Write("Please enter a number: ");
+            string answer = Console.ReadLine();
+
+            x = int.Parse(answer);
+            numbers.Add(x);
+            count = count + 1;
+        }while (x != 0);
+
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            if (numbers[i] > max)
+            {
+                max = numbers[i];
+            }
+            sum = sum + numbers[i];
+
+            
+        }
+        float average = (sum / count);
+
+        Console.WriteLine($"The Average was: {average}");
+        Console.WriteLine($"the Max was {max}");
+        Console.WriteLine($"the Sum is {sum}");
+        
+
     }
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        static void DisplayMessage()
+        {
+            Console.WriteLine("welcom to the program");
+        }
+        static string username()
+        {
+            Console.Write("What is your name: ");
+            string answer = Console.ReadLine();
+            return answer;
+        }
+        static int UserNumber()
+        {
+            Console.Write("what is your user number: ");
+            string number = Console.ReadLine();
+            int answer = int.Parse(number);
+            return answer;
+        }
+        static int SquareNumber(int UserNumber)
+        {
+            int calc = UserNumber * UserNumber;
+            return calc;
+        }
+        static void DisplayResult(string name, int SquaredNumber)
+        {
+            Console.WriteLine($"Your name is {name} And your number squared is {SquaredNumber}");
+        }
+        DisplayMessage();
+        string x = username();
+        int y = UserNumber();
+        int z = SquareNumber (y);
+        DisplayResult (x, z);
+
+
     }
 }
